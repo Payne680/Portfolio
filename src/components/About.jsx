c1cad5c1b6db860fb6c838bde2185ad98eec7cbd
PENN JR <pennjunior680@gmail.com>
2023-10-19 18:19:56 +0100

    Working on some updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
         sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
         quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
         vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus!
+        obcaecati voluptatum aliquam corrupti, suscipit
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
